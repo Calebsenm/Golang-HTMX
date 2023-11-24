57cfd12f68e631c25b1fd29761bad8137a50fa04
Caleb Seña Melo <91163501+Calebsenm@users.noreply.github.com>
2023-11-24 06:58:56 +0000

    Finish

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
+
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"time"
-
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type Film struct{
@@ -15,32 +17,67 @@ type Film struct{
 }
 
 
-func h1(w http.ResponseWriter , r *http.Request){
-	tmpl := template.Must(template.ParseFiles("index.html"));
-	films := map[string][]Film{
-		"films":{
-			{Title: "Blade Runner", Director:  "Ridley Scoot" },
-		},
-
+func OpenCvs() [][] string{
+	file , err := os.Open("./Films.cvs");
+	if err != nil{
+		fmt.Println(err);
 	}
+
+	reader := csv.NewReader(file);
+	data , _ := reader.ReadAll();
+	defer file.Close();
+	
+	return data
+}
+
+
+func home(w http.ResponseWriter , r *http.Request){
+	tmpl := template.Must(template.ParseFiles("index.html"));
+	
+	data := OpenCvs()
+	var films = map[string][] Film {}	
+	
+	for i:= 0 ; i < len(data); i++ {
+		films["films"] = append(films["films"],	Film{Title:  data[i][0] , Director:  data[i][1] })
+	} 
+
 	tmpl.Execute(w, films);
 }
 
-func h2(w http.ResponseWriter , r *http.Request){
+func add(w http.ResponseWriter , r *http.Request){
 	time.Sleep( 1 * time.Second )
 	title := r.PostFormValue("title");
 	director := r.PostFormValue("director");
+
+	theData := OpenCvs();
+
+	file , err := os.Create("./Films.cvs");
+	defer file.Close();
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	theData = append(theData, []string{title, director})
+	
+	writer := csv.NewWriter(file);
+	err = writer.WriteAll(theData);
+
+	if err != nil{
+		log.Fatal("Error " , err)
+	}
 
 	tpml := template.Must(template.ParseFiles("index.html"))
 	tpml.ExecuteTemplate(w ,"film-list-element", Film{Title: title , Director: director})
 } 
 
+
 func main(){
 
 	fmt.Println("Hello Again");	
 
-	http.HandleFunc("/", h1);
-	http.HandleFunc("/add-film/", h2);
+	http.HandleFunc("/", home );
+	http.HandleFunc("/add-film/", add);
 
-	log.Fatal( http.ListenAndServe(":8081", nil ))
+	log.Fatal( http.ListenAndServe(":8084", nil ))
 }
