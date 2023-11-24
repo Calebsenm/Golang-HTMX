35e21751d4a8e085ff2ff1a1aaa80075ae6b9a8f
Caleb Seña Melo <91163501+Calebsenm@users.noreply.github.com>
2023-11-24 03:34:29 +0000

    OK

--- main.go
+++ main.go
@@ -1,1 +1,46 @@
-G
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Film struct{
+	Title     string
+	Director  string 
+}
+
+
+func h1(w http.ResponseWriter , r *http.Request){
+	tmpl := template.Must(template.ParseFiles("index.html"));
+	films := map[string][]Film{
+		"films":{
+			{Title: "Blade Runner", Director:  "Ridley Scoot" },
+		},
+
+	}
+	tmpl.Execute(w, films);
+}
+
+func h2(w http.ResponseWriter , r *http.Request){
+	time.Sleep( 1 * time.Second )
+	title := r.PostFormValue("title");
+	director := r.PostFormValue("director");
+
+	tpml := template.Must(template.ParseFiles("index.html"))
+	tpml.ExecuteTemplate(w ,"film-list-element", Film{Title: title , Director: director})
+} 
+
+func main(){
+
+	fmt.Println("Hello Again");	
+
+	http.HandleFunc("/", h1);
+	http.HandleFunc("/add-film/", h2);
+
+	log.Fatal( http.ListenAndServe(":8081", nil ))
+}
---
+++ main.md
@@ -0,0 +1,1 @@
+The Reference is this -> https://www.youtube.com/watch?v=F9H6vYelYyU
